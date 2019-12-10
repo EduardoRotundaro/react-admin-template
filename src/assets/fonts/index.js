7292b6e781b40c317d6eae80cd5a03262e75152f
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-12-10 18:57:21 -0300

    :construction: build issue fix + assets configuration

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faAngry } from '@fortawesome/free-regular-svg-icons';
+import { faBacon } from '@fortawesome/free-solid-svg-icons';
+
+import '../css/fonts.css';
+
+library.add(faReact, faAngry, faBacon);
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/app.css';
+
+import './assets/fonts';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
