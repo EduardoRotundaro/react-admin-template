b54aafcd98c93b3a454ddd24267b8b6f311b76c1
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-12-11 09:59:47 -0300

    :sparkles: navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { Fragment } from 'react';
+
+export default props => {
+
+    const getDropdownItems = (items=[]) => items.map(item => (
+        <Fragment>
+            <a className="dropdown-item" href={item.ref}>{item.name}</a>
+            <div className="dropdown-divider"></div>
+        </Fragment>
+    ));   
+
+    function getMainItems () {
+        const items = props.navItens || [];
+        return items.map(item => {
+            if(Array.isArray(item.ref))
+                return (
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" 
+                            href="#" 
+                            id="navbarDropdown" 
+                            role="button" 
+                            data-toggle="dropdown" 
+                            aria-haspopup="true" 
+                            aria-expanded="false"
+                        >
+                            {item.name}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            {getDropdownItems(item.ref)}
+                        </div>
+                    </li>
+                );
+            else
+                return (
+                    <li class="nav-item active">
+                        <a class="nav-link" href={item.ref}>{item.name}</a>
+                    </li>
+                );
+        });
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href={}>
+                <img src={} alt="Logo" className="navbar-logo"/>
+            </a>
+            <button className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    {getMainItems()}
+                </ul>
+            </div>
+        </nav>
+    );
+}
