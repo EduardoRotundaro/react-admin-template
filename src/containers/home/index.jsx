0f2699820b9f9acc913d261295b9c5edca97000f
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-12-12 11:38:18 -0300

    :art: SASS structure

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default props => (
-    <h2>Home</h2>
+    <span className="badge badge-success">Success</span>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/app.css';
 import './assets/css/app.scss';
-
 import './assets/fonts';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
